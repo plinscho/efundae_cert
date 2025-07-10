f59a6913f4f17b9f32414978a7c944abc799eaa1
plinscho <paul.linschoten@gmail.com>
2025-07-10 16:24:42 +0200

    starting php

---
+++ backend_4/php/first.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>First PHP</title>
+</head>
+<body>
+	<h1>Welcome to the first php</h1>
+	<p>Current date and time is: <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+</html>
